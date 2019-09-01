5031c36193fd272b4714a0459e74ad7bd3e84d5e
Sk-Riyaz <skriyaz09@gmail.com>
2019-09-01 19:41:54 +0530

    Updates

--- precache-manifest.3feba0c77cc367009902e8270b7f1cce.js
+++ precache-manifest.3feba0c77cc367009902e8270b7f1cce.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "17bcd40d880a82104b936ce7568e11b9",
+    "revision": "4298cd81107dc8844619fba3b1623c9b",
     "url": "/monsters-rolodex/index.html"
   },
   {
-    "revision": "337ad758ca4a6e9525ef",
+    "revision": "dceee190e8207178d4cd",
     "url": "/monsters-rolodex/static/css/main.fb2a45e5.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
   },
   {
-    "revision": "337ad758ca4a6e9525ef",
-    "url": "/monsters-rolodex/static/js/main.45201c72.chunk.js"
+    "revision": "dceee190e8207178d4cd",
+    "url": "/monsters-rolodex/static/js/main.001f0271.chunk.js"
   },
   {
     "revision": "c7f991e6cb4746d6bd30",
